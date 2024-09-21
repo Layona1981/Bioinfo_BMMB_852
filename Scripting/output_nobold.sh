2a168af5ab26e4d9d07884cd5b6e89af51eb8fba
Lina Sallam <143635269+Layona1981@users.noreply.github.com>
2024-09-21 17:37:37 -0400

    Add files via upload

---
+++ Scripting/output_nobold.sh
@@ -0,0 +1,13 @@
+# Read the content of the file
+with open('output_nobold.sh', 'r') as file:
+    content = file.read()
+
+# Remove all occurrences of '######' from the beginning of each line
+content = content.replace('######', '')
+
+# Write the modified content back to the file
+with open('output_nobold.sh', 'w') as file:
+    file.write(content)
+
+print("All occurrences of '######' have been removed from 'output_nobold.sh'.")
+
