8f6c0c0d7b9aa18e2a8d8e3c8f554a4a61e00aa7
Lina Sallam <143635269+Layona1981@users.noreply.github.com>
2024-09-23 10:03:33 -0400

    Update accipiter_script.sh

--- Scripting/accipiter_script.sh
+++ Scripting/accipiter_script.sh
@@ -9,35 +9,11 @@ set -uex
 # URLs for downloading the files
 URL_GFF="https://ftp.ensembl.org/pub/current_gff3/accipiter_nisus/Accipiter_nisus.Accipiter_nisus_ver1.0.112.abinitio.gff3.gz"
 
-# Names of the files
-GFF_FILE="Accipiter_nisus.Accipiter_nisus_ver1.0.112.abinitio.gff3.gz"
-UNZIPPED_GFF_FILE="Accipiter_nisus.Accipiter_nisus_ver1.0.112.abinitio.gff3"
 
-# Output files for extracted features
-GENES_FILE="accipiter_nisus_genes.gff"
-CDS_FILE="accipiter_nisus_CDS.gff"
-MRNA_FILE="accipiter_nisus_mRNA.gff"
-EXONS_FILE="accipiter_nisus_exons.gff"
-
-# ------ ALL THE ACTIONS FOLLOW ------
-
-# Download the genomic file using curl
-curl -o ${GFF_FILE} ${URL_GFF}
 
 # Unzip the file
-gunzip ${GFF_FILE}
+gunzip ${ccipiter_nisus.Accipiter_nisus_ver1.0.112.abinitio.gff3.gz}
 
-# Extract genes from the GFF file
-cat ${UNZIPPED_GFF_FILE} | awk '$3 == "gene"' > ${GENES_FILE}
-
-# Extract CDS from the GFF file
-cat ${UNZIPPED_GFF_FILE} | awk '$3 == "CDS"' > ${CDS_FILE}
-
-# Extract mRNA from the GFF file
-cat ${UNZIPPED_GFF_FILE} | awk '$3 == "mRNA"' > ${MRNA_FILE}
-
-# Extract exons from the GFF file
-cat ${UNZIPPED_GFF_FILE} | awk '$3 == "exon"' > ${EXONS_FILE}
 
 # Print the number of genes
 echo "Number of genes:"
