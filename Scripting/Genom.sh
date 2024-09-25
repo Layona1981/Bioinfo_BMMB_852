7ed3b1c7c64c24ffb1084ddc2161a0432b65e047
Lina Sallam <143635269+Layona1981@users.noreply.github.com>
2024-09-25 14:04:41 -0400

    Update Genom.sh

--- Scripting/Genom.sh
+++ Scripting/Genom.sh
@@ -19,3 +19,17 @@ echo "Occurrences of 'gene': $gene_count"
 # List the most common feature types
 echo "Most common feature types:"
 grep -v '^#' Accipiter_nisus.Accipiter_nisus_ver1.0.112.gff3 | cut -f3 | sort | uniq -c | sort -nr | head -n 10
+
+
+# Most annotated feature types
+echo "45991 genes"
+echo "309995 exon"
+echo "301420 CDS"
+echo "133065 biological_region"
+echo "44975 region"
+echo "27007 mRNA"
+echo "18324 five_prime_UTR"
+echo "17271 gene"
+echo "14159 three_prime_UTR"
+echo "754 ncRNA_gene"
+echo "433 lnc_RNA"
